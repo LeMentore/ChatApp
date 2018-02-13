3a009ad2db53f3781bfcb0a4c87886550c47e36c
Charlie Frye <klayton.alan@gmail.com>
2018-02-13 20:58:53 +0500

    created connect server with client

---
+++ client/src/util/ws.js
@@ -0,0 +1,15 @@
+((wsUrl) => {
+    let ws;
+    ws = new WebSocket(wsUrl);
+
+    ws.onopen = () => {
+        console.log('ws open');
+    }
+
+    ws.onmessage = (message) => {
+        console.log(message.data);
+    }
+
+    return ws
+
+})('ws://localhost:3001')
